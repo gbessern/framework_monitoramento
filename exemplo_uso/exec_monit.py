2feebf35b7c394f8f3179a9f2ad231ab9dc06df3
gbessern <gbessern@gmail.com>
2024-12-01 11:53:44 -0300

    Remove files covered by .gitignore

--- exemplo_uso/exec_monit.py
+++
@@ -1,6 +0,0 @@
-percentis_baseline = 'hive_metastore.default.percentis_baseline'
-percentis_prod = 'hive_metastore.default.percentis_prod'
-psi_table = 'hive_metastore.default.psi'
-ks_table =  'hive_metastore.default.ks'
-pct_fraude_table = 'hive_metastore.default.pct_fraude'
-
